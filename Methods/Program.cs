d15ae73302bf96612ed77351e3619ea69777ca92
BurakUmutAlkan <burumalkan96@gmail.com>
2023-01-28 01:12:59 +0300

    Methods part 1

---
+++ Methods/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Add();
+            Add();
+            Add();
+            Add();
+
+            var result = Add2(20, 30);
+            Console.WriteLine(result);
+
+            result = Add3(20);
+            Console.WriteLine(result);
+
+            int numberOne = 20;
+            int numberTwo = 100;
+            result = Add4(numberOne, numberTwo);
+            Console.WriteLine(numberOne);
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+
+        static void Add()
+        {
+            Console.WriteLine("Added!");
+        }
+
+        static int Add2(int numberOne, int numberTwo)
+        {
+            return numberOne + numberTwo;
+        }
+
+        static int Add3(int numberOne, int numberTwo = 30)
+        {
+            return numberOne + numberTwo;
+        }
+
+        static int Add4(int numberOne, int numberTwo)
+        {
+            numberOne = 30;
+            return numberOne + numberTwo;
+        }
+    }
+}
